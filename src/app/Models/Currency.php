605868558be77c977bcd14af99d205aa7380500c
Mahdi <mahdidev70@gmail.com>
2023-12-30 00:31:28 +0330

    fix: solve model name problme

--- src/app/Models/Currency.php
+++ src/app/Models/Currency.php
@@ -5,7 +5,7 @@ namespace PostorShop\CurrencyModules\app\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Chapter extends Model
+class Currency extends Model
 {
     use HasFactory;
 
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,1 +1,7 @@
 <?php
+
+// Route::middleware(['auth', 'web', 'CheckAdmin'])->prefix('admin')->group(function () {
+//     Route::group(['prefix' => 'currency'], function () {
+//         Route::get('/index', [CurrencyController::class, 'index'])->name('currency.index');
+//     });
+// });
